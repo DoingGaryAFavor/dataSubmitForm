51c839c65a26d1f437816b5132aba3ef46e7b1aa
Brenton Ang <notnerb.8808@gmail.com>
2015-08-08 08:41:51 -0700

    Updated deal and restaurant availability. New web submission form design.

--- parse/cloud/main.js
+++ parse/cloud/main.js
@@ -9,3 +9,211 @@ app.use(express.bodyParser());
 app.get('/', function(req, res) {
   res.render('main', {});
 });
+
+Parse.Cloud.define("upVote", function(request, response) {
+  Parse.Cloud.useMasterKey();
+  var deviceQuery = new Parse.Query(Parse.Object.extend("User"));
+  deviceQuery.equalTo("deviceId", request.params.deviceId);
+  deviceQuery.first({
+    success: function (user) {
+      if (typeof user == "undefined") {
+        var userClass = Parse.Object.extend("User");
+        var user = new userClass();
+        user.save(
+          {deviceId: request.params.deviceId, upVotes: [], downVotes: [],
+            username: request.params.deviceId, password: ""},{
+          success:function(user) {
+            Parse.Cloud.run("upVote", request.params, {
+              success: function(result) {
+                response.success(result);
+              }, error: function(error) {
+                response.error(error);
+              }
+            });
+          }, error:function(user, error) {
+            response.error("Unable to create user" + error.message);
+          }
+        });
+      } else if (user.get("upVotes").indexOf(request.params.objectId) == -1) {
+        /*if (user.get("downVotes").indexOf(request.params.objectId) != -1) {
+          return false;
+        } else {*/
+          upVote(user, request.params.objectId, {
+            success: function(result) {
+              response.success(result);
+            }, error: function(error) {
+              response.error(error);
+            }
+          });
+       // }
+      } else {
+        response.error("User has already up voted the deal: " + request.params.objectId);
+      }
+    }, error: function (error) {
+      response.error("Failed to find device " + request.params.deviceId + error.code);
+    }
+  });
+})
+
+Parse.Cloud.define("downVote", function(request, response) {
+  Parse.Cloud.useMasterKey();
+  var deviceQuery = new Parse.Query(Parse.Object.extend("User"));
+  deviceQuery.equalTo("deviceId", request.params.deviceId);
+  deviceQuery.first({
+    success: function (user) {
+      if (typeof user == "undefined") {
+        var userClass = Parse.Object.extend("User");
+        var user = new userClass();
+        user.save(
+          {deviceId: request.params.deviceId, upVotes: [], downVotes: [],
+            username: request.params.deviceId, password: ""},{
+          success:function(user ) {
+            Parse.Cloud.run("downVote", request.params, {
+              success: function(result) {
+                response.success(result);
+              }, error: function(error) {
+                response.error(error);
+              }
+            });
+          }, error:function(user, error) {
+            response.error("Unable to create user" + error.message);
+          }
+        });
+      } else if (user.get("downVotes").indexOf(request.params.objectId) == -1) {
+        /*if (user.get("upVotes").indexOf(request.params.objectId) != -1) {
+          return;
+        } else {*/
+          downVote(user, request.params.objectId, {
+            success: function(result) {
+              response.success(result);
+            }, error: function(error) {
+              response.error(error);
+            }
+          });
+        //}
+      } else {
+        response.error("User has already down voted the deal: " + request.params.objectId);
+      }
+    }, error: function (error) {
+      response.error("Failed to find device " + request.params.deviceId + error.code);
+    }
+  });
+})
+
+Parse.Cloud.define("undoUpVote", function(request, response) {
+  Parse.Cloud.useMasterKey();
+  var deviceQuery = new Parse.Query(Parse.Object.extend("User"));
+  deviceQuery.equalTo("deviceId", request.params.deviceId);
+  deviceQuery.first({
+    success: function (user) {
+      undoUpVote(user, request.params.objectId, {
+        success: function(result) {
+          response.success("Successfully reverted up vote on deal " + request.params.objectId);
+        }, error: function(error) {
+          response.error(error);
+        }
+      });
+    }, error: function (error) {
+      response.error("Failed to revert up vote on deal " + deal.id + error.code);
+    }
+  });
+})
+
+Parse.Cloud.define("undoDownVote", function(request, response) {
+  Parse.Cloud.useMasterKey();
+  var deviceQuery = new Parse.Query(Parse.Object.extend("User"));
+  deviceQuery.equalTo("deviceId", request.params.deviceId);
+  deviceQuery.first({
+    success: function (user) {
+      undoDownVote(user, request.params.objectId, {
+        success: function(result) {
+          response.success("Successfully reverted down vote on deal " + request.params.objectId);
+        }, error: function(error) {
+          response.error(error);
+        }
+      });
+    }, error: function (error) {
+      response.error("Failed to revert down vote on deal " + deal.id + error.code);
+    }
+  });
+})
+
+function upVote (user, objectId, options) {
+  user.add("upVotes", objectId);
+  user.save();
+  updateUpVote(objectId, 1, {
+    success: function(result) {
+      options.success(result);
+    }, error: function(error) {
+      options.error(error);
+    }
+  });
+}
+
+function undoUpVote (user, objectId, options) {
+  user.remove("upVotes", objectId);
+  user.save();
+  updateUpVote(objectId, -1, {
+    success: function(result) {
+      options.success(result);
+    }, error: function(error) {
+      options.error(error);
+    }
+  });
+}
+
+function downVote (user, objectId, options) {
+  user.add("downVotes", objectId);
+  user.save();
+  updateDownVote(objectId, 1, {
+    success: function(result) {
+      options.success(result);
+    }, error: function(error) {
+      options.error(error);
+    }
+  });
+}
+
+function undoDownVote (user, objectId, options) {
+  user.remove("downVotes", objectId);
+  user.save();
+  updateDownVote(objectId, -1, {
+    success: function(result) {
+      options.success(result);
+    }, error: function(error) {
+      options.error(error);
+    }
+  });
+}
+
+function updateUpVote (objectId, value, options) {
+  var query = new Parse.Query(Parse.Object.extend("deals"));
+  query.equalTo("objectId", objectId);
+  query.get().then(function(deal) {
+    deal.increment("upVotes", value);
+    var ratingCalculation = 0;
+    if (deal.get("upVotes") != 0) 
+      ratingCalculation = (100 * (deal.get("upVotes") / (deal.get("upVotes") + deal.get("downVotes"))));
+    deal.set("rating", ratingCalculation);
+    deal.save();
+    options.success("Successfully updated up vote value of deal " + objectId + " by " + value);
+  }, function(error) {
+    options.error("Could not update up vote value of deal " + objectId);
+  });
+}
+
+function updateDownVote (objectId, value, options) {
+  var query = new Parse.Query(Parse.Object.extend("deals"));
+  query.equalTo("objectId", objectId);
+  query.get().then(function(deal) {
+    deal.increment("downVotes", value);
+    var ratingCalculation = 0;
+    if (deal.get("upVotes") != 0)
+      ratingCalculation = (100 * (deal.get("downVotes") / (deal.get("upVotes") + deal.get("downVotes"))));
+    deal.set("rating", ratingCalculation);
+    deal.save();
+    options.success("Successfully updated down vote value of deal " + objectId + " by " + value);
+  }, function(error) {
+    options.error("Could not update down vote value of deal " + objectId);
+  });
+}
--- parse/public/main.js
+++ parse/public/main.js
@@ -1,3 +1,11 @@
+var constants = {
+  'Month' : '111111111111',
+  'DaysOfMonth' : '0000000000000000000000000000000',
+  'WeeksOfMonth' : '1111',
+  'DayOfWeek' : '0000000',
+  'NullTime' : '4095'
+}
+
 // appliction id, javascript id
 Parse.initialize(
     "zjbuJvWrvzgdpDvRnHejLD008hLGf6zHua5nCGvq",
@@ -23,6 +31,14 @@ function initialize() {
 }
 google.maps.event.addDomListener(window, 'load', initialize); 
 
+/*
+function showHide(control) {
+  if (document.getElementById('categorical').checked)
+    document.getElementById('categoricalItem').style.visibility = 'visible';
+  else
+    document.getElementById('categoricalItem').style.visibility = 'hidden';
+}*/
+
 function toTitleCase(str)
 {
     return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
@@ -37,6 +53,7 @@ function processForm(e) {
     var Restaurant = Parse.Object.extend("restaurants");
     var restaurantQuery = new Parse.Query(Restaurant);
 
+    /*
     var restaurantAvailabilityObject = { 
       sundaySt: document.getElementById('sundaySt').value, sundayEn: document.getElementById('sundayEn').value,
       mondaySt: document.getElementById('mondaySt').value, mondayEn: document.getElementById('mondayEn').value,
@@ -45,7 +62,7 @@ function processForm(e) {
       thursdaySt: document.getElementById('thursdaySt').value, thursdayEn: document.getElementById('thursdayEn').value,
       fridaySt: document.getElementById('fridaySt').value, fridayEn: document.getElementById('fridayEn').value,
       saturdaySt: document.getElementById('saturdaySt').value, saturdayEn: document.getElementById('saturdayEn').value
-    };
+    };*/
 
     var vegan = document.getElementById('vegan');
     var veganValue = false;
@@ -67,19 +84,181 @@ function processForm(e) {
     if (alcohol.checked)
       alcoholValue = true;
 
-    var wheelchairAccessible = document.getElementById('wheelchairAccessible');
-    var wheelchairAccessibleValue = false;
-    if (wheelchairAccessible.checked)
-      wheelchairAccessibleValue = true;
-
     var checkboxes = document.getElementsByName('dealtype');
     var checkboxesChecked = [];
 
     for (var i = 0; i<checkboxes.length; i++) {
-       if (checkboxes[i].checked) {
-          checkboxesChecked.push(checkboxes[i].value);
-       }
+     if (checkboxes[i].checked)
+        checkboxesChecked.push(checkboxes[i].value);
     }
+
+    var restaurantDays1CheckBoxes = document.getElementsByName('restaurantDays1');
+    var restaurantDays1Checked = [];
+    var daysOfWeekBitValue = 0;
+    var day = 64;
+
+    for (var i = 0; i<restaurantDays1CheckBoxes.length; i++) {
+      if (restaurantDays1CheckBoxes[i].checked)
+        daysOfWeekBitValue = daysOfWeekBitValue | day;
+      day = day >> 1;
+    }
+
+    var restaurantRecurrence1Value = constants.Month.concat(constants.DaysOfMonth).concat(constants.WeeksOfMonth).concat(daysOfWeekBitValue.toString(2));
+
+    var restaurantDays2CheckBoxes = document.getElementsByName('restaurantDays2');
+    var restaurantDays2Checked = [];
+    var daysOfWeekBitValue2 = 0;
+    var day2 = 64;
+
+    for (var i = 0; i<restaurantDays2CheckBoxes.length; i++) {
+      if (restaurantDays2CheckBoxes[i].checked)
+        daysOfWeekBitValue2 = daysOfWeekBitValue2 | day2;
+      day2 = day2 >> 1;
+    }
+
+    if (daysOfWeekBitValue2 == 0)
+      var restaurantRecurrence2Value = null;
+    else 
+      restaurantRecurrence2Value = constants.Month.concat(constants.DaysOfMonth).concat(constants.WeeksOfMonth).concat(daysOfWeekBitValue2.toString(2));
+
+    var dealDays1CheckBoxes = document.getElementsByName('dealDays1');
+    var dealDays1Checked = [];
+    var dealDaysOfWeekBitValue = 0;
+    var day = 64;
+
+    for (var i = 0; i<dealDays1CheckBoxes.length; i++) {
+      if (dealDays1CheckBoxes[i].checked)
+        dealDaysOfWeekBitValue = dealDaysOfWeekBitValue | day;
+      day = day >> 1;
+    }
+
+    var dealRecurrence1Value = constants.Month.concat(constants.DaysOfMonth).concat(constants.WeeksOfMonth).concat(dealDaysOfWeekBitValue.toString(2));
+
+    var dealDays2CheckBoxes = document.getElementsByName('dealDays2');
+    var dealDays2Checked = [];
+    var dealDaysOfWeekBitValue2 = 0;
+    var day = 64;
+
+    for (var i = 0; i<dealDays2CheckBoxes.length; i++) {
+      if (dealDays2CheckBoxes[i].checked)
+        dealDaysOfWeekBitValue2 = dealDaysOfWeekBitValue2 | day;
+      day2 = day2 >> 1;
+    }
+
+    if (dealDaysOfWeekBitValue2 == 0)
+      var dealRecurrence2Value = null;
+    else
+      dealRecurrence2Value = constants.Month.concat(constants.DaysOfMonth).concat(constants.WeeksOfMonth).concat(dealDaysOfWeekBitValue2.toString(2));
+
+    function pad(n, width, z) {
+      z = z || '0';
+      n = n + '';
+      return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+    }
+
+    var restaurantFirstOpen1 = parseInt(document.getElementById('restaurantFirstOpen1').value).toString(2);
+    var restaurantFirstOpen1LeadingZeroes = pad(restaurantFirstOpen1, 12);
+    if (document.getElementById('restaurantLastOpen1').value == "")
+      var restaurantLastOpen1 = parseInt(constants.NullTime).toString(2);
+    else
+      var restaurantLastOpen1 = parseInt(document.getElementById('restaurantLastOpen1').value).toString(2);
+    var restaurantLastOpen1LeadingZeroes = pad(restaurantLastOpen1, 12);
+    var openTime1 = restaurantLastOpen1LeadingZeroes.concat(restaurantFirstOpen1LeadingZeroes);
+    if (document.getElementById('restaurantLastOpen1').value == "" && document.getElementById('restaurantFirstOpen1').value == "")
+      var openTime1Value = null;
+    else
+      var openTime1Value = parseInt(openTime1, 2);
+
+    if (document.getElementById('restaurantFirstClose1').value == "")
+      var restaurantFirstClose1 = parseInt(constants.NullTime).toString(2);
+    else
+      var restaurantFirstClose1 = parseInt(document.getElementById('restaurantFirstClose1').value).toString(2);
+    var restaurantFirstClose1LeadingZeroes = pad(restaurantFirstClose1, 12);
+    var restaurantLastClose1 = parseInt(document.getElementById('restaurantLastClose1').value).toString(2);
+    var restaurantLastClose1LeadingZeroes = pad(restaurantLastClose1, 12);
+    var closeTime1 = restaurantLastClose1LeadingZeroes.concat(restaurantFirstClose1LeadingZeroes);
+    if (document.getElementById('restaurantFirstClose1').value == "" && document.getElementById('restaurantLastClose1').value == "")
+      var closeTime1Value = null;
+    else
+      var closeTime1Value = parseInt(closeTime1, 2);
+
+    var restaurantFirstOpen2 = parseInt(document.getElementById('restaurantFirstOpen2').value).toString(2);
+    var restaurantFirstOpen2LeadingZeroes = pad(restaurantFirstOpen2, 12);
+    if (document.getElementById('restaurantLastOpen2').value == "")
+      var restaurantLastOpen2 = parseInt(constants.NullTime).toString(2);
+    else
+      var restaurantLastOpen2 = parseInt(document.getElementById('restaurantLastOpen2').value).toString(2);
+    var restaurantLastOpen2LeadingZeroes = pad(restaurantLastOpen2, 12);
+    var openTime2 = restaurantLastOpen2LeadingZeroes.concat(restaurantFirstOpen2LeadingZeroes);
+    if (document.getElementById('restaurantLastOpen2').value == "" && document.getElementById('restaurantFirstOpen2').value == "")
+      var openTime2Value = null;
+    else
+      var openTime2Value = parseInt(openTime2, 2);
+
+    if (document.getElementById('restaurantFirstClose2').value == "")
+      var restaurantFirstClose2 = parseInt(constants.NullTime).toString(2);
+    else
+      var restaurantFirstClose2 = parseInt(document.getElementById('restaurantFirstClose2').value).toString(2);
+    var restaurantFirstClose2LeadingZeroes = pad(restaurantFirstClose2, 12);
+    var restaurantLastClose2 = parseInt(document.getElementById('restaurantLastClose2').value).toString(2);
+    var restaurantLastClose2LeadingZeroes = pad(restaurantLastClose2, 12);
+    var closeTime2 = restaurantLastClose2LeadingZeroes.concat(restaurantFirstClose2LeadingZeroes);
+    if (document.getElementById('restaurantFirstClose2').value == "" && document.getElementById('restaurantLastClose2').value == "")
+      var closeTime2Value = null;
+    else
+      var closeTime2Value = parseInt(closeTime2, 2);
+
+    var dealFirstOpen1 = parseInt(document.getElementById('dealFirstOpen1').value).toString(2);
+    var dealFirstOpen1LeadingZeroes = pad(dealFirstOpen1, 12);
+    if (document.getElementById('dealLastOpen1').value == "")
+      var dealLastOpen1 = parseInt(constants.NullTime).toString(2);
+    else
+      var dealLastOpen1 = parseInt(document.getElementById('dealLastOpen1').value).toString(2);
+    var dealLastOpen1LeadingZeroes = pad(dealLastOpen1, 12);
+    var dealOpenTime1 = dealLastOpen1LeadingZeroes.concat(dealFirstOpen1LeadingZeroes);
+    if (document.getElementById('dealLastOpen1').value == "" && document.getElementById('dealFirstOpen1').value == "")
+      var dealOpenTime1Value = null;
+    else
+      var dealOpenTime1Value = parseInt(dealOpenTime1, 2);
+
+    if (document.getElementById('dealFirstClose1').value == "")
+      var dealFirstClose1 = parseInt(constants.NullTime).toString(2);
+    else
+      var dealFirstClose1 = parseInt(document.getElementById('dealFirstClose1').value).toString(2);
+    var dealFirstClose1LeadingZeroes = pad(dealFirstClose1, 12);
+    var dealLastClose1 = parseInt(document.getElementById('dealLastClose1').value).toString(2);
+    var dealLastClose1LeadingZeroes = pad(dealLastClose1, 12);
+    var dealCloseTime1 = dealLastClose1LeadingZeroes.concat(dealFirstClose1LeadingZeroes);
+    if (document.getElementById('dealLastClose1').value == "" && document.getElementById('dealFirstClose1').value == "")
+      var dealCloseTime1Value = null;
+    else
+      var dealCloseTime1Value = parseInt(dealCloseTime1, 2);
+
+    var dealFirstOpen2 = parseInt(document.getElementById('dealFirstOpen2').value).toString(2);
+    var dealFirstOpen2LeadingZeroes = pad(dealFirstOpen2, 12);
+    if (document.getElementById('dealLastOpen2').value == "")
+      var dealLastOpen2 = parseInt(constants.NullTime).toString(2);
+    else
+      var dealLastOpen2 = parseInt(document.getElementById('dealLastOpen2').value).toString(2);
+    var dealLastOpen2LeadingZeroes = pad(dealLastOpen2, 12);
+    var dealOpenTime2 = dealLastOpen2LeadingZeroes.concat(dealFirstOpen2LeadingZeroes);
+    if (document.getElementById('dealLastOpen2').value == "" && document.getElementById('dealFirstOpen2').value == "")
+      var dealOpenTime2Value = null;
+    else
+      var dealOpenTime2Value = parseInt(dealOpenTime2, 2);
+
+    if (document.getElementById('dealFirstClose2').value == "")
+      var dealFirstClose2 = parseInt(constants.NullTime).toString(2);
+    else
+      var dealFirstClose2 = parseInt(document.getElementById('dealFirstClose2').value).toString(2);
+    var dealFirstClose2LeadingZeroes = pad(dealFirstClose2, 12);
+    var dealLastClose2 = parseInt(document.getElementById('dealLastClose2').value).toString(2);
+    var dealLastClose2LeadingZeroes = pad(dealLastClose2, 12);
+    var dealCloseTime2 = dealLastClose2LeadingZeroes.concat(dealFirstClose2LeadingZeroes);
+    if (document.getElementById('dealLastClose2').value == "" && document.getElementById('dealFirstClose2').value == "")
+      var dealCloseTime2Value = null;
+    else
+      var dealCloseTime2Value = parseInt(dealCloseTime2, 2);
 
     var dealTagArrayLowerCase = checkboxesChecked.toString().toLowerCase();
     var dealTagArraySplit = dealTagArrayLowerCase.toString().split(",");
@@ -99,6 +278,8 @@ function processForm(e) {
 
     var dealItem = toTitleCase(document.getElementById('dealItem').value.toString());
     deal.set("item", dealItem);
+    var categoricalItem = toTitleCase(document.getElementById('categoricalItem').value.toString());
+    deal.set("category", categoricalItem);
     var dealPrimaryTag = toTitleCase(document.getElementById('dealPrimaryTag').value.toString());;
     deal.set("primaryTag", dealPrimaryTag);
     deal.set("fineprint", toTitleCase(document.getElementById('dealFinePrint').value));
@@ -109,12 +290,18 @@ function processForm(e) {
     deal.set("vegan", veganValue);
     deal.set("vegetarian", vegetarianValue);
     deal.set("glutenFree", glutenFreeValue);
-    deal.set("submittedBy", document.getElementById('submittedBy').value);
+    deal.set("submittedBy", toTitleCase(document.getElementById('submittedBy').value));
     deal.set("downVotes", 0);
     deal.set("upVotes", 0);
     deal.set("rating", 0);
     deal.set("flags", 0);
-    deal.set("sundaySt", parseInt(document.getElementById('sundayDealSt').value));
+    deal.set("recurrence1", dealRecurrence1Value);
+    deal.set("openTime1", dealOpenTime1Value);
+    deal.set("closeTime1", dealCloseTime1Value);
+    deal.set("recurrence2", dealRecurrence2Value);
+    deal.set("openTime2", dealOpenTime2Value);
+    deal.set("closeTime2", dealCloseTime2Value);
+    /*deal.set("sundaySt", parseInt(document.getElementById('sundayDealSt').value));
     deal.set("sundayEn", parseInt(document.getElementById('sundayDealEn').value));
     deal.set("mondaySt", parseInt(document.getElementById('mondayDealSt').value));
     deal.set("mondayEn", parseInt(document.getElementById('mondayDealEn').value));
@@ -127,7 +314,7 @@ function processForm(e) {
     deal.set("fridaySt", parseInt(document.getElementById('fridayDealSt').value));
     deal.set("fridayEn", parseInt(document.getElementById('fridayDealEn').value));
     deal.set("saturdaySt", parseInt(document.getElementById('saturdayDealSt').value));
-    deal.set("saturdayEn", parseInt(document.getElementById('saturdayDealEn').value));
+    deal.set("saturdayEn", parseInt(document.getElementById('saturdayDealEn').value));*/
     var point = new Parse.GeoPoint({latitude: parseFloat(document.getElementById('restaurantLatitude').value),
                  longitude: parseFloat(document.getElementById('restaurantLongitude').value)});
 
@@ -151,10 +338,15 @@ function processForm(e) {
               restaurant.set("city", document.getElementById('restaurantCity').value);
               restaurant.set("state", document.getElementById('restaurantState').value)
               restaurant.set("zipCode", document.getElementById('restaurantZipCode').value);
-              restaurant.set("availability", restaurantAvailabilityObject);
-              restaurant.set("wheelchairAccess", wheelchairAccessibleValue);
+              // restaurant.set("availability", restaurantAvailabilityObject);
               restaurant.set("twentyOnePlus", alcoholValue);
-              restaurant.set("submittedBy", document.getElementById('submittedBy').value);
+              restaurant.set("recurrence1", restaurantRecurrence1Value);
+              restaurant.set("openTime1", openTime1Value);
+              restaurant.set("closeTime1", closeTime1Value);
+              restaurant.set("recurrence2", restaurantRecurrence2Value);
+              restaurant.set("openTime2", openTime2Value);
+              restaurant.set("closeTime2", closeTime2Value);
+              restaurant.set("submittedBy", toTitleCase(document.getElementById('submittedBy').value));
               deal.set("restaurantId", restaurant);
               deal.save(null, {
                 success: function(deal){ },
